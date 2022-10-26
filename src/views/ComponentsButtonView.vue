5b7699da246c54a2ed8691a4fdc747244e00a293
Zhen Shen <zeroshen@vt.edu>
2022-10-26 16:50:52 -0400

    Modify the components page

--- src/views/ComponentsButtonView.vue
+++ src/views/ComponentsButtonView.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-.home-page {
+.page {
   display: flex;
 }
 .components-list {
   width: 20%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  padding-left: 50px;
 }
-.game-button-container {
+.code-container {
   width: 80%;
+  display: flex;
+  flex-direction: column;
+  padding: 50px;
+  justify-content: center;
 }
 code {
   padding: 10px;
@@ -20,13 +26,14 @@ code {
 }
 </style>
 <template>
-  <div class="home-page">
+  <div class="page">
     <section class="components-list">
-      <router-link to="../components/Button">Button</router-link>
-      <router-link to="../components/Alerts">Alerts</router-link>
-      <router-link to="../components/Input">Input</router-link>
+      <li><router-link to="../components/Button">Button</router-link></li>
+      <li><router-link to="../components/Alerts">Alerts</router-link></li>
+      <li><router-link to="../components/Inputs">Inputs</router-link></li>
+      <li><router-link to="../components/Image">Images</router-link></li>
     </section>
-    <section class="game-button-container">
+    <section class="code-container">
       <pre>
         <code>
          1234
