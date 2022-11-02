5fa5db789c9dd485eb646d8b9cc8e04d305c45b7
Yi Lu <59351375+yiee38@users.noreply.github.com>
2022-11-02 15:24:07 -0400

    card tuner

--- src/components/CardTuner.vue
+++ src/components/CardTuner.vue
@@ -54,8 +54,7 @@ export default defineComponent({
   -moz-border-radius: 4px;
   -webkit-border-radius: 4px;
   border-radius: 4px;
-  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14),
-    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   color: black;
   margin: 8px;
   min-width: 290px;
