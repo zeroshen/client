c9f546162b84dd68d63898ffd752f68dba110bde
Zhen Shen <zeroshen@vt.edu>
2022-11-09 15:11:04 -0500

    Add description for game

--- src/views/GamesLevelOneView.vue
+++ src/views/GamesLevelOneView.vue
@@ -1,9 +1,22 @@
 <script setup></script>
 
-<style></style>
+<style>
+.page-view {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
 
 <template>
-  <router-link to="../game/1/quiz">
-    <button>Go to Quiz</button>
-  </router-link>
+  <section class="page-view">
+    <div>Descriptions for Vue</div>
+    <div>
+      Mary wants to implement a website. Try to fill TODO in the template.
+    </div>
+    <router-link to="../game/1/quiz">
+      <button>Go to Quiz</button>
+    </router-link>
+  </section>
 </template>
